4d2ce57e9bcc10e07bb41c6c3c97d6937490c549
biancavictorino <biancamarie.victorino@gmail.com>
2024-06-16 15:30:33 +0800

    adjust input

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -21,9 +21,9 @@ const Header = ({onAddTask}) => {
             <h1 className='text-tertiary-color font-bold text-5xl'>Check</h1>
             <h1 className='text-secondary-color font-bold text-5xl'>List</h1>
         </div>
-        <div className='w-screen flex items-center justify-center lg:w-screen lg:px-96'>
+        <div className='px-8 w-screen flex items-center justify-center'>
             <form action="" onSubmit={handleSubmit} className='flex flex-row gap-2'>
-                <input className='px-3 rounded-lg h-10 border-2 border-secondary-color bg-secondary-color bg-opacity-20 text-secondary-color lg:w-full' required placeholder='Add a new task' type="text" value={title} onChange={onChangeTitle} />
+                <input className='px-3 rounded-lg h-10 border-2 border-secondary-color bg-secondary-color bg-opacity-20 text-secondary-color lg:w-96' required placeholder='Add a new task' type="text" value={title} onChange={onChangeTitle} />
                 <button className='bg-custom-gradient px-6 rounded-lg text-sm font-semibold flex flex-row items-center gap-2'>
                     Create
                 </button>
