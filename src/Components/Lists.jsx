2857673dbdeb65be01f30d64564443a1b286bd42
biancavictorino <biancamarie.victorino@gmail.com>
2024-06-19 21:54:48 +0800

    modified task container

--- src/Components/Lists.jsx
+++ src/Components/Lists.jsx
@@ -4,14 +4,14 @@ import { MdDelete } from "react-icons/md";
 
 function Lists ({ task, onComplete, onDelete }) {
   return (
-    <div className='p-4 my-2 bg-secondary-color bg-opacity-60 border-2 border-secondary-color shadow-lg rounded-lg flex flex-row items-center justify-between'>
-      <div className='flex flex-row items-center gap-4'>
-        <button className='w-6 border-2 border-primary-color bg-transparent h-6 rounded-full' onClick={() => onComplete(task.id)}>
+    <div className='p-4 my-2 h-auto bg-secondary-color bg-opacity-60 border-2 border-secondary-color shadow-lg rounded-lg flex flex-row flex-grow items-center justify-between'>
+      <div className='flex flex-row items-center gap-4 flex-grow'>
+        <button className='w-6 h-6 border-2 border-primary-color bg-transparent rounded-full flex items-center justify-center flex-shrink-0' onClick={() => onComplete(task.id)}>
           {task.isCompleted ? <BsFillCheckCircleFill size={20} className='border-primary-color rounded-full'/> : <div className='bg-primary-color' />}
         </button>
-        <p className={task.isCompleted ? `line-through opacity-60` : ""}>{task.title}</p>
+        <p className={`flex flex-grow break-all ${task.isCompleted ? `line-through opacity-60` : ""}`}>{task.title}</p>
       </div>
-      <div>
+      <div className='flex-shrink-0 pl-4'>
         <MdDelete className='h-6 w-6' onClick={() => onDelete(task.id)}/>
       </div>
     </div>
